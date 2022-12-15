3a00fb198eca14ae107ecacdf1547789b7afcde4
DESKTOP-20GG9V8\Gilbo <Andrew.gilbert@y7mail.com>
2022-12-15 16:19:03 +1100

    Add comments

--- Assets/Scripts/Enemies/Gas.cs
+++ Assets/Scripts/Enemies/Gas.cs
@@ -9,22 +9,21 @@ public class Gas : MonoBehaviour
     public float Damaged = 20;
     float time;
     public float TimeTaken = 1;
-    void Start()
-    {
-        
-    }
+    
 
     void Update()
     {   
+        // if time = 1 it sets time to 0 and it becomes true 
         time += Time.deltaTime;
         if(ingas && time >= TimeTaken)
         {
+        // sends damaged to player 
         Temp.GetComponent<PlayerStats>().TakeDamaged(Damaged);
         time = 0;
         }
     }
   
-  
+  // enables and disable the gas to deal damaged 
    void OnTriggerEnter(Collider other)
     {
         Temp = other;
